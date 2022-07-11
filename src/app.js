a56931cd3073d599dfa0e7a12c83cba905e5fbe9
Clasanba <clasanba@gmail.com>
2022-07-11 10:11:38 +0000

    Segundo intento subida ejercicio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[generateRandom(who.length)];
+  let que = what[generateRandom(what.length)];
+  let cuando = when[generateRandom(when.length)];
+  var element = document.getElementById("the-excuse"); //busca un id de un html
+  element.textContent = quien + " " + que + " " + "my homework" + " " + cuando; // cambia el texto
+  return console.log(quien + " " + que + " " + "my homework" + " " + cuando);
 };
+
+function generateRandom(max) {
+  return Math.floor(Math.random() * max);
+}
